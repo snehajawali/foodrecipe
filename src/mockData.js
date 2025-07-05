ff4c2824c06416aa5ae9c198be1b762914bc7546
snehajawali <136260873+snehajawali@users.noreply.github.com>
2025-07-05 19:33:05 +0530

    Create mockData.js

---
+++ src/mockData.js
@@ -0,0 +1,89 @@
+const mockRecipes = [
+    {
+      recipe: {
+        label: "Grilled Chicken Salad",
+        calories: 430,
+        image: "https://hips.hearstapps.com/hmg-prod/images/grilled-chicken-salad-lead-6628169550105.jpg?resize=1200:*",
+        ingredients: [
+          { text: "2 chicken breasts" },
+          { text: "1 head of romaine lettuce" },
+          { text: "Cherry tomatoes" },
+          { text: "Olive oil" },
+          { text: "Salt and pepper" }
+        ],
+      },
+    },
+    {
+      recipe: {
+        label: "Panner butter masala",
+        calories: 720,
+        image: "https://myfoodstory.com/wp-content/uploads/2021/07/restaurant-style-paneer-butter-masala-2-500x500.jpg",
+        ingredients: [
+          { text: "200g panner" },
+          { text: "100g masala paste" },
+          { text: "Tomato sauce" },
+          { text: "Onion" },
+          { text: "Garlic" }
+        ],
+      },
+    },
+    {
+      recipe: {
+        label: "Avocado Toast",
+        calories: 320,
+        image: "https://www.spendwithpennies.com/wp-content/uploads/2022/09/Avocado-Toast-SpendWithPennies-1.jpg",
+        ingredients: [
+          { text: "2 slices whole grain bread" },
+          { text: "1 ripe avocado" },
+          { text: "Lemon juice" },
+          { text: "Chili flakes" },
+          { text: "Salt" }
+        ],
+      },
+    },
+    {
+      recipe: {
+        label: "Pancakes with Berries",
+        calories: 580,
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTJD-3Ee2QYyRTHKhGm8b8ndSbOjIYbWws6Mw&s",
+        ingredients: [
+          { text: "1 cup flour" },
+          { text: "1 egg" },
+          { text: "1 cup milk" },
+          { text: "Maple syrup" },
+          { text: "Mixed berries" }
+        ],
+      },
+    },
+    {
+      recipe: {
+        label: "Chicken Curry",
+        calories: 690,
+        image: "https://www.whiskaffair.com/wp-content/uploads/2021/10/Andhra-Chicken-Curry-2-3.jpg",
+        ingredients: [
+          { text: "2 chicken thighs" },
+          { text: "Curry paste" },
+          { text: "Coconut milk" },
+          { text: "Rice" },
+          { text: "Coriander" }
+        ],
+      },
+    },
+    {
+      recipe: {
+        label: "Vegetable pulao",
+        calories: 410,
+        image: "https://www.cubesnjuliennes.com/wp-content/uploads/2019/10/Veg-Pulao.jpg",
+        ingredients: [
+          { text: "Mixed vegetables" },
+          { text: "Soy sauce" },
+          { text: "Ginger" },
+          { text: "Garlic" },
+          { text: "rice" }
+        ],
+      },
+    }
+  ];
+  
+  export default mockRecipes;
+  
